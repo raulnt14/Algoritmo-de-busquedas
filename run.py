c6a9452c2e2032ee5fd86b1237f35c8691ec8fcf
Raúl Núñez <raul.nunez101@alu.ulpgc.es>
2023-12-02 20:32:12 +0000

    Implementación de todos los tests para los algoritmos BFS y DFS

--- run.py
+++ run.py
@@ -4,7 +4,10 @@ import search
 import time
 
 ab = search.GPSProblem('A', 'B', search.romania)
-
+oe = search.GPSProblem('O', 'E', search.romania)
+gz = search.GPSProblem('G', 'Z', search.romania)
+nd = search.GPSProblem('N', 'D', search.romania)
+mf = search.GPSProblem('M', 'F', search.romania)
 
 print("Generados: " + str(search.breadth_first_graph_search(ab)[2]))
 print("Visitados: " + str(search.breadth_first_graph_search(ab)[1]))
@@ -14,14 +17,75 @@ print(search.breadth_first_graph_search(ab)[0].path())
 time.sleep(0.1)
 print(time.time() - inicioB - 0.1)
 
+print("Generados: " + str(search.breadth_first_graph_search(oe)[2]))
+print("Visitados: " + str(search.breadth_first_graph_search(oe)[1]))
+print("Costo total: " + str(search.breadth_first_graph_search(oe)[0].path_cost))
+inicioB = time.time()
+print(search.breadth_first_graph_search(oe)[0].path())
+time.sleep(0.1)
+print(time.time() - inicioB - 0.1)
+
+print("Generados: " + str(search.breadth_first_graph_search(gz)[2]))
+print("Visitados: " + str(search.breadth_first_graph_search(gz)[1]))
+print("Costo total: " + str(search.breadth_first_graph_search(gz)[0].path_cost))
+inicioB = time.time()
+print(search.breadth_first_graph_search(gz)[0].path())
+time.sleep(0.1)
+print(time.time() - inicioB - 0.1)
+
+print("Generados: " + str(search.breadth_first_graph_search(nd)[2]))
+print("Visitados: " + str(search.breadth_first_graph_search(nd)[1]))
+print("Costo total: " + str(search.breadth_first_graph_search(nd)[0].path_cost))
+inicioB = time.time()
+print(search.breadth_first_graph_search(nd)[0].path())
+time.sleep(0.1)
+print(time.time() - inicioB - 0.1)
+
+print("Generados: " + str(search.breadth_first_graph_search(mf)[2]))
+print("Visitados: " + str(search.breadth_first_graph_search(mf)[1]))
+print("Costo total: " + str(search.breadth_first_graph_search(mf)[0].path_cost))
+inicioB = time.time()
+print(search.breadth_first_graph_search(mf)[0].path())
+time.sleep(0.1)
+print(time.time() - inicioB - 0.1)
+
+
 print("Generados: " + str(search.depth_first_graph_search(ab)[2]))
 print("Visitados: " + str(search.depth_first_graph_search(ab)[1]))
 print("Costo total: " + str(search.depth_first_graph_search(ab)[0].path_cost))
 inicioD = time.time()
 print(search.depth_first_graph_search(ab)[0].path())
 time.sleep(0.1)
-print(time.time() - inicioB - 0.1)
+print(time.time() - inicioD - 0.1)
 
-# Result:
-# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
-# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
+print("Generados: " + str(search.depth_first_graph_search(oe)[2]))
+print("Visitados: " + str(search.depth_first_graph_search(oe)[1]))
+print("Costo total: " + str(search.depth_first_graph_search(oe)[0].path_cost))
+inicioD = time.time()
+print(search.depth_first_graph_search(oe)[0].path())
+time.sleep(0.1)
+print(time.time() - inicioD - 0.1)
+
+print("Generados: " + str(search.depth_first_graph_search(gz)[2]))
+print("Visitados: " + str(search.depth_first_graph_search(gz)[1]))
+print("Costo total: " + str(search.depth_first_graph_search(gz)[0].path_cost))
+inicioD = time.time()
+print(search.depth_first_graph_search(gz)[0].path())
+time.sleep(0.1)
+print(time.time() - inicioD - 0.1)
+
+print("Generados: " + str(search.depth_first_graph_search(nd)[2]))
+print("Visitados: " + str(search.depth_first_graph_search(nd)[1]))
+print("Costo total: " + str(search.depth_first_graph_search(nd)[0].path_cost))
+inicioD = time.time()
+print(search.depth_first_graph_search(nd)[0].path())
+time.sleep(0.1)
+print(time.time() - inicioD - 0.1)
+
+print("Generados: " + str(search.depth_first_graph_search(mf)[2]))
+print("Visitados: " + str(search.depth_first_graph_search(mf)[1]))
+print("Costo total: " + str(search.depth_first_graph_search(mf)[0].path_cost))
+inicioD = time.time()
+print(search.depth_first_graph_search(mf)[0].path())
+time.sleep(0.1)
+print(time.time() - inicioD - 0.1)
